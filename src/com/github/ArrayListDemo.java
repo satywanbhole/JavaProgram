16e6765c1339892d61eed099308a81b83b4f7e26
satywanbhole <satywanbhole@gmail.com>
2023-06-12 23:45:19 +0530

    first code : added arraylist program

---
+++ src/com/github/ArrayListDemo.java
@@ -0,0 +1,18 @@
+package com.github;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		List<Integer> list= new ArrayList<Integer>();
+		list.add(12);
+		list.add(13);
+		list.add(14);
+		
+		System.out.println(list);
+
+	}
+
+}
